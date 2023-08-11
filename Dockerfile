1d7c5f4fc6f8f14480ed45774a368dbe92008edf
Alex Towers <johnatowers@gmail.com>
2023-08-11 16:00:15 -0400

    added .env file, parameterized secrets, wrote dockerfile, added gitignore

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python3:latest
+
+# # Install dependencies
+# RUN apt-get update && \
+#  apt-get -y install python3
+
+ADD main.py in_stock_notifier.py
+
+# Install python3 dependencies
+RUN pip3 install requests \
+ && pip3 install BeautifulSoup4 \
+ && pip3 install twilio \
+ && pip3 install python-dotenv
+
+EXPOSE 80
+
+CMD [ "python3", "./in_stock_notifier.py" ]
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 from twilio.rest import Client
+from dotenv import load_dotenv
 
-account_sid = "<twilio account id>"
-auth_token = "<twilio account token>"
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
-url = "<url of website you want to check stock on>"
+url = "https://www.lego.com/en-us/product/lego-star-wars-darth-vader-keyring-850996"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # expression on parsed to find the buy button, this will differ on every webpage
-parsed = soup.find('span', attrs={"data-default-text": True}).text
+parsed = soup.find('p', attrs={"class": "Text__BaseText-sc-13i1y3k-0 bAqfGh ProductOverviewstyles__AvailabilityStatus-sc-1a1az6h-11 jRMAfF"}).text
 inStock = parsed.strip()
 
 print(inStock)
 
 # Simple expression checking if the button from stock != Sold out
-if inStock != "Sold Out":
+if inStock != "Sold out":
     message = client.messages.create(
-        body="<message i.e Your item is back in stock.>",
-        from_="<your twilio number>",
-        to="<your phone number>"
+        body="The LEGO® Star Wars™ Darth Vader™ Key Chain is back in stock",
+        from_= os.getenv('TWILIO_PHONE_NUMBER'),
+        to= os.getenv('YOUR_PHONE_NUMBER')
     )
 else:
+    print("The LEGO® Star Wars™ Darth Vader™ Key Chain is out of stock"),
     exit(0)
