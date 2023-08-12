ad70df46edf519c9d7d87b6fcfee38bf94fc6075
johnatowers <36558773+johnatowers@users.noreply.github.com>
2023-08-11 20:45:58 -0400

    Update main.py to actually read the env variables

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
 url = "https://www.lego.com/en-us/product/lego-star-wars-darth-vader-keyring-850996"
@@ -25,8 +25,8 @@ print(inStock)
 if inStock != "Sold out":
     message = client.messages.create(
         body="The LEGO® Star Wars™ Darth Vader™ Key Chain is back in stock",
-        from_= os.getenv('TWILIO_PHONE_NUMBER'),
-        to= os.getenv('YOUR_PHONE_NUMBER')
+        from_= os.environ.get('TWILIO_PHONE_NUMBER'),
+        to= os.environ.get('YOUR_PHONE_NUMBER')
     )
 else:
     print("The LEGO® Star Wars™ Darth Vader™ Key Chain is out of stock"),
